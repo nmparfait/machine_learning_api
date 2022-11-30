e316fc99021b13a43a150d373cd15ef1023bdff9
nmparfait <mbamu.developer@gmail.com>
2022-11-30 01:08:15 -0500

    authentication built with flask basic-auth in order to get access only with the correct username and password

--- machine_learning_api_with_py.py
+++ machine_learning_api_with_py.py
@@ -1,5 +1,6 @@
 import warnings
 from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 from textblob import TextBlob
 from sklearn.linear_model import LinearRegression
 import pickle
@@ -8,9 +9,12 @@ import pickle
 modelo = pickle.load(open('modelo.sav', 'rb'))
 
 colunas = ['tamanho', 'ano', 'garagem']
+
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'parfait'
+app.config['BASIC_AUTH_PASSWORD'] = 'mlops'
 
-
+basic_auth = BasicAuth(app)
 @app.route('/')
 def home():
     return "Minha primeira API em python"
@@ -18,6 +22,7 @@ def home():
 
 # url para ponto de accesso
 @app.route("/sentimento/<frase>")
+@basic_auth.required
 def sentimento(frase):
     tb = TextBlob(frase)
     tb_en = tb.translate(to="en")
@@ -26,6 +31,7 @@ def sentimento(frase):
     return "polaridade: {}".format(polaridade)
 
 @app.route("/cotacao/", methods=['POST'])
+@basic_auth.required
 def cotacao(tamanho):
     dados = request.get_json()
     dados_input = [dados[col] for col in colunas]

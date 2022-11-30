1328db5b5fa253abff71ce3d5139a7232a5c13de
nmparfait <mbamu.developer@gmail.com>
2022-11-29 23:25:40 -0500

    predict model build with the api

--- machine_learning_api_with_py.py
+++ machine_learning_api_with_py.py
@@ -1,7 +1,24 @@
 import warnings
-
+import pandas as pd
 from flask import Flask
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 from textblob import TextBlob
+
+df = pd.read_csv('casas.csv')
+
+# escolher apenas as colunas que usaremos para criar o nosso modelo
+colunas = ['tamanho', 'preco']
+df = df[colunas]
+
+# valor preditor
+X = df.drop('preco', axis=1)
+y = df['preco']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state = 42)
+
+modelo = LinearRegression()
+modelo.fit(X_train, y_train)
 
 app = Flask(__name__)
 
@@ -19,6 +36,11 @@ def sentimento(frase):
     warnings.warn("this is deprecated", DeprecationWarning, 2)
     polaridade = tb_en.sentiment.polarity
     return "polaridade: {}".format(polaridade)
+
+@app.route("/cotacao/<int:tamanho>")
+def cotacao(tamanho):
+    preco = modelo.predict([[tamanho]])
+    return str(preco)
 
 
 app.run(debug=True)

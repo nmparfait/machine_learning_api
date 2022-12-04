2d66e6b9c307caee84d8bdbf6779eb2e9e3ea06f
nmparfait <mbamu.developer@gmail.com>
2022-12-03 20:55:04 -0500

    request access and authentication done

--- machine_learning_api_with_py.py
+++ machine_learning_api_with_py.py
@@ -1,45 +1,36 @@
-import warnings
 from flask import Flask, request, jsonify
 from flask_basicauth import BasicAuth
 from textblob import TextBlob
 from sklearn.linear_model import LinearRegression
 import pickle
 
-# use the serialization with pickle in order to upload file with speed
-modelo = pickle.load(open('modelo.sav', 'rb'))
-
-colunas = ['tamanho', 'ano', 'garagem']
+colunas = ['tamanho','ano','garagem']
+modelo = pickle.load(open('modelo.sav','rb'))
 
 app = Flask(__name__)
-app.config['BASIC_AUTH_USERNAME'] = 'parfait'
-app.config['BASIC_AUTH_PASSWORD'] = 'mlops'
+app.config['BASIC_AUTH_USERNAME'] = 'app'
+app.config['BASIC_AUTH_PASSWORD'] = 'app'
 
 basic_auth = BasicAuth(app)
+
 @app.route('/')
 def home():
-    return "Minha primeira API em python"
+    return "Minha primeira API."
 
-
-# url para ponto de accesso
-@app.route("/sentimento/<frase>")
+@app.route('/sentimento/<frase>')
 @basic_auth.required
 def sentimento(frase):
     tb = TextBlob(frase)
-    tb_en = tb.translate(to="en")
-    warnings.warn("this is deprecated", DeprecationWarning, 2)
+    tb_en = tb.translate(from_lang='pt', to='en')
     polaridade = tb_en.sentiment.polarity
     return "polaridade: {}".format(polaridade)
 
-@app.route("/cotacao/", methods=['POST'])
+@app.route('/cotacao/', methods=['POST'])
 @basic_auth.required
-def cotacao(tamanho):
+def cotacao():
     dados = request.get_json()
     dados_input = [dados[col] for col in colunas]
     preco = modelo.predict([dados_input])
     return jsonify(preco=preco[0])
 
-
 app.run(debug=True)
-
-#restart a execução automaticamente.
-#app.run(debug=True)
